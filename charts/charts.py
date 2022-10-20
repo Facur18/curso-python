1e36a9badd879a5d3a2a587300afeb2c9fc1192b
Facundo Rocha <facurochadaniel@gmail.com>
2022-10-20 19:51:54 -0300

    add docs

---
+++ charts/charts.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as pylot
+
+def generate_pie_chart():
+    labels = ["A","b","c"]
+    values = [200, 100 ,120]
+
+    fig, ax = pylot.subplots()
+    ax.pie(values, labels=labels)
+    pylot.savefig(".pie.png")
+    pylot.close()
+
+
+
+
